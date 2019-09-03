0680713936d5d35e215f91d25f3be3d87f73ef4f
zhangqijohn <792543749@qq.com>
2019-09-03 17:36:46 +0800

    add es data

--- basis/typeScript/lesson1.js
+++ basis/typeScript/lesson1.js
@@ -1,6 +1,20 @@
 var hello = "hello";
 console.log(hello);
-var num = 2; // 类型推断为 number
-console.log("num 变量的值为 " + num);
-num = "12"; // 编译错误
-console.log(num);
+var str = '1';
+var str2 = str; //str、str2 是 string 类型
+console.log(str2);
+var global_num = 1; // 全局变量
+var Numbers = /** @class */ (function () {
+    function Numbers() {
+        this.num_val = 2; // 类变量
+    }
+    Numbers.prototype.storeNum = function () {
+        var local_num = 4; // 局部变量
+    };
+    Numbers.s_num = 3; // 静态变量
+    return Numbers;
+}());
+console.log("全局变量为: " + global_num);
+console.log(Numbers.s_num); // 静态变量
+var obj = new Numbers();
+console.log("类变量: " + obj.num_val);
--- basis/typeScript/lesson1.ts
+++ basis/typeScript/lesson1.ts
@@ -1,3 +1,20 @@
 const hello : string = "hello"
 console.log(hello)
 
+var str = '1'
+var str2:number = <number> <any> str   //str、str2 是 string 类型
+console.log(str2)
+
+
+var global_num:number = 1  // 全局变量
+class Numbers {
+    num_val  = 2 // 类变量
+    static s_num = 3 // 静态变量
+    storeNum():void {
+        var local_num = 4 // 局部变量
+    }
+}
+console.log("全局变量为: "+global_num)
+console.log(Numbers.s_num)   // 静态变量
+var obj = new Numbers();
+console.log("类变量: "+obj.num_val)
